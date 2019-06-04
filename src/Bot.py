0df16a032147dd014c2b0d617ce2dead1657e986
Ashish D'Souza <ashish.dsouza@students.polytech.k12.de.us>
2019-06-04 16:00:33 -0400

    Update Bot.py

--- src/Bot.py
+++ src/Bot.py
@@ -7,14 +7,14 @@ import praw
 from time import sleep
 
 
-with open("settings.txt", "r") as file:
-  settings = file.readlines()
-
-delay = int(settings[0][settings[0].index(":") + 1:].strip())
-paragraph = "".join(settings[1:])
-
 reddit = praw.Reddit("bot")
 
 while True:
+  with open("settings.txt", "r") as file:
+    settings = file.readlines()
+  
+  delay = int(settings[0][settings[0].index(":") + 1:].strip())
+  paragraph = "".join(settings[1:])
+  
   reddit.submission("bkz2do").reply(paragraph)
   sleep(delay * 60)
