93590eaf9886cb21a2ace99deaddde9964a81337
Ashish D'Souza <ashish.dsouza@students.polytech.k12.de.us>
2019-06-04 15:16:05 -0400

    Create Bot.py

---
+++ src/Bot.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# Bot.py
+# Ashish D'Souza (computer_geek64)
+# June 4th, 2019
+
+import praw
+from time import sleep
+
+
+with open("settings.txt", "r") as file:
+  settings = file.readlines()
+
+delay = int(settings[0][settings[0].index(":") + 1:].strip())
+paragraph = "".join(settings[1:])
+
+reddit = praw.Reddit("bot")
+
+while True:
+  reddit.submission("bkz2do").reply(paragraph)
+  sleep(delay * 60)
