04a82c20a3d553b1492fff34701cd04ef7f82cfe
computer-geek64 <ashish.dsouza@students.polytech.k12.de.us>
2019-06-05 18:38:35 -0400

    Update

--- src/Bot.py
+++ src/Bot.py
@@ -1,23 +1,32 @@
 #!/usr/bin/python3
 # Bot.py
 # Ashish D'Souza (computer_geek64)
-# June 4th, 2019
+# June 5th, 2019
 
 import os
+import git
 import praw
 from time import sleep
 
 
 os.chdir(os.path.dirname(__file__))
 
+git.Repo(__file__[:__file__.index("rocket-league-reddit-bot") + 24]).remotes.origin.pull()
+
 reddit = praw.Reddit("bot")
+unauthenticated_reddit = praw.Reddit("unauthenticated_bot")
+
 
 while True:
-  with open("settings.txt", "r") as file:
-    settings = file.readlines()
-  
-  delay = int(settings[0][settings[0].index(":") + 1:].strip())
-  paragraph = "".join(settings[1:])
-  
-  reddit.submission("bkz2do").reply(paragraph)
-  sleep(delay * 60)
+    with open("settings.txt", "r") as file:
+        settings = file.readlines()
+
+    delay = int(settings[0][settings[0].index(":") + 1:].strip())
+    paragraph = "".join(settings[1:])
+
+    print(unauthenticated_reddit.redditor(str(reddit.user.me())).submissions.new().next().author)
+
+    exit(0)
+
+    reddit.submission("bkz2do").reply(paragraph)
+    sleep(delay * 60)
