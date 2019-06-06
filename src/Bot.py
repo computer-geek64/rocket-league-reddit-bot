d76f8ef1651fcada19874dc77428dd1609981e91
computer-geek64 <ashish.dsouza@students.polytech.k12.de.us>
2019-06-05 21:51:24 -0400

    Update

--- src/Bot.py
+++ src/Bot.py
@@ -12,8 +12,8 @@ from datetime import datetime
 
 os.chdir(os.path.dirname(__file__))
 
-if sum(1 for c in git.Repo(__file__[:__file__.index("rocket-league-reddit-bot") + 24]).iter_commits("origin/master..master")):
-    print(sum(1 for c in git.Repo(__file__[:__file__.index("rocket-league-reddit-bot") + 24]).iter_commits("origin/master..master")))
+if sum(1 for c in git.Repo(__file__[:__file__.index("rocket-league-reddit-bot") + 24]).iter_commits("master..origin/master")):
+    print(sum(1 for c in git.Repo(__file__[:__file__.index("rocket-league-reddit-bot") + 24]).iter_commits("master..origin/master")))
     git.Repo(__file__[:__file__.index("rocket-league-reddit-bot") + 24]).remotes.origin.pull()
     print("Updating...")
     exit(0)
